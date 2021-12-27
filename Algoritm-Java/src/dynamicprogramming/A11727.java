b5ff382544ac2cb76bf2818573d95524e5dd18eb
HYERIN <gpfls0506@gmail.com>
2021-12-27 21:38:42 +0900

    2xn타일링2(11727)

---
+++ Algoritm-Java/src/dynamicprogramming/A11727.java
@@ -0,0 +1,31 @@
+package dynamicprogramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class A11727 {
+//2xn 타일링 2
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());		
+		long[] dp = new long[n+1];
+		
+		dp[1] = 1;
+		if(n>1) {
+			dp[2] = 3;
+		}
+		for(int i=3;i<=n;i++) {
+			dp[i] = dp[i-1]+dp[i-2]*2;
+			if(dp[i]>10007) {
+				dp[i] %= 10007;
+			}
+		}
+		
+		//DP문제에서 종종 결과를 어떤 수로 나눈 결과를 출력하라는 내용이 들어가있는 경우가 많다.
+		//이는 단지 결괏값이 굉장히 커질 수 있기 때문에 그런 것이다.
+		System.out.println(dp[n]);
+
+	}
+
+}
