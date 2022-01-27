b7dd132685a482335af606e49f8c43fda4b78897
오혜린 <gpfls0506@gmail.com>
2022-01-27 15:03:46 +0900

    update

--- Algoritm-Java/src/greedy/BJ_1931_회의실배정.java
+++ Algoritm-Java/src/greedy/BJ_1931_회의실배정.java
@@ -9,6 +9,8 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BJ_1931_회의실배정 {
+    //백준 실버2
+    //서로 겹치지 않는 활동에 대해 종료시간이 빠르면 더 많은 활동을 선택할 수 있는 시간이 많아진다.
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,24 +28,21 @@ public class BJ_1931_회의실배정 {
         meeting.sort(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                if(o1[0]==o2[0]){
-                    return o1[1]-o2[1];
+                if(o1[1]==o2[1]){
+                    return o1[0]-o2[0];
                 }
-                return o1[0]-o2[0];
+                return o1[1]-o2[1];
             }
         });
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.offer(meeting.get(0)[1]);
-        int count = 1;
-        for(int i=1;i< meeting.size();i++){
-            if(pq.peek()<=meeting.get(i)[0]){
-                pq.poll();
+        int count = 0;
+        int prev_end = 0;
+        for(int i=0;i<n;i++){
+            if(prev_end<=meeting.get(i)[0]){
+                prev_end = meeting.get(i)[1];
                 count++;
             }
-            pq.offer(meeting.get(i)[1]);
         }
-
         System.out.println(count);
 
     }
