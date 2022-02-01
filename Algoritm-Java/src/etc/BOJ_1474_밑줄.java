ad2586bc413a89d43053c744ac104a0e4758a9b3
오혜린 <gpfls0506@gmail.com>
2022-02-02 00:05:37 +0900

    update

--- Algoritm-Java/src/etc/BOJ_1474_밑줄.java
+++ Algoritm-Java/src/etc/BOJ_1474_밑줄.java
@@ -1,4 +1,29 @@
 package etc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class BOJ_1474_밑줄 {
+    //백준 실버1
+
+    static int n, m;
+    static ArrayList<String> list= new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        for(int i=0;i<n;i++){
+            list.add(br.readLine());
+        }
+        //-------------입력완료-----------------
+
+    }
 }
