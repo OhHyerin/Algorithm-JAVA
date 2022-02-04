45d22d2727d26db70ee31136930ec3e5d54a472d
오혜린 <gpfls0506@gmail.com>
2022-02-04 22:28:46 +0900

    update

--- Algoritm-Java/src/etc/BOJ_5427_불.java
+++ Algoritm-Java/src/etc/BOJ_5427_불.java
@@ -3,7 +3,7 @@ package etc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -11,117 +11,139 @@ import java.util.StringTokenizer;
 public class BOJ_5427_불 {
     //백준 골드4
     //BFS?
+    /* 탈출구 2개일 때 안풀림
+1
+7 6
+###.###
+#.*...#
+#..@..#
+#.....#
+#.....#
+###.###
+     */
 
-    static int T;
-    static int W, H;
-    static int[][] map;
-    static int[] rdir = {-1, 1, 0, 0}; //상하좌우
-    static int[] cdir = {0, 0, -1, 1};
+    static int R, C;
+    static char [][] map;
+    static Queue<Pos> fires;
     static int min;
+    //상, 하, 좌, 우
+    static int dr[] = {-1, 1, 0, 0};
+    static int dc[] = {0, 0, -1, 1};
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
-        T = Integer.parseInt(br.readLine());
-        for(int t=1;t<=T;t++) {
-            st = new StringTokenizer(br.readLine());
-            W = Integer.parseInt(st.nextToken());
-            H = Integer.parseInt(st.nextToken());
-            map = new int[H+2][W+2]; //탈출구까지 합해서 초기화
-            Pos sang = null; //상근이 위치 초기화
-            Queue<Pos> fire = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
 
-            for (int h = 1; h <= H; h++) {
-                //?? : char배열 받을 땐 StringTokenizer로 해결 못하는지?
-                //(St.nextToken()).charAt(0);
-                String str = br.readLine();
-                for (int w = 1; w <= W; w++) {
-                    char c = str.charAt(w-1);
-                    if(c=='@'){
-                        //상근이 시작 위치
-                        sang = new Pos(h,w,0);
-                    }else if(c=='*'){
-                        //불
-                        fire.offer(new Pos(h,w));
-                        map[h][w]= -1; //방문처리
-                    }else if(c=='#'){
-                        map[h][w] = -1; //방문처리
+        int T = Integer.parseInt(br.readLine());
+        for(int t=1;t<=T;t++){
+            st = new StringTokenizer(br.readLine());
+            C = Integer.parseInt(st.nextToken());
+            R = Integer.parseInt(st.nextToken());
+
+            map = new char[R+2][C+2];
+            for(int i=0;i<R+2;i++){
+                for(int j=0;j<C+2;j++){
+                    map[i][j] = 'X';
+                }
+            }
+            fires = new LinkedList<>();
+
+
+            int curR=0, curC=0;
+
+            for(int i=1;i<=R;i++){
+                String line = br.readLine();
+                for(int j=1;j<=C;j++){
+                    map[i][j] = line.charAt(j-1);
+                    if(map[i][j]=='@'){
+                        curR = i;
+                        curC = j;
+                    } else if(map[i][j]=='*'){
+//                        System.out.println("불 생성! i : "+i +"    j : "+j);
+                        fires.add(new Pos(i, j));
                     }
                 }
             }
-            //---------------입력완료------------------------
+
+//            for(char[] i:map){
+//                System.out.println(Arrays.toString(i));
+//            }
+
             min = Integer.MAX_VALUE;
-            bfs(sang, fire, map);
+            maze(curR, curC);
 
-            //MAX_VALUE 그대로면 끝에 도달하지 못한것이므로 IMPOSSIBLE출력
+
             if(min != Integer.MAX_VALUE){
-                System.out.println(min);
-            } else{
-                System.out.println("IMPOSSIBLE");
+                sb.append(min).append("\n");
+            }else{
+                sb.append("IMPOSSIBLE\n");
             }
-
-
-
         }
+        System.out.println(sb);
+
+
     }
 
-    private static void bfs(Pos person, Queue<Pos> fire, int[][] visited){
-        Queue<Pos> queue = new LinkedList<>();
+    static void maze(int r, int c){
+        Queue<Pos> q = new LinkedList<>();
+        q.offer(new Pos(r, c, 0));
 
-        //초기화
-        visited[person.r][person.c] = 1; //상근이 처음위치 방문처리 o
-        queue.offer(person);
-
-        //상근이의 위치를 담은 queue가 빌때까지 수행
-        while(!queue.isEmpty()){
+        while(!q.isEmpty()){
             //불 먼저 퍼뜨리기
-            //시간 별로 퍼뜨리기 위해 초기 담겨있던 불의 개수만큼만 진행하고
-            //새로 이동한 불은 다음 반복때 퍼트린다.
-            for(int i=0;i<fire.size();i++){
-                //불의 갯수만큼
-                Pos f = fire.poll();
+            // 시간별로 퍼뜨리기 위해 초기 담겨있던 불의 개수만큼만 진행하고
+            //새로 이동한 불은 다음 반복때 퍼트리기
+            for(int i=0, end = fires.size();i<end;i++){ //여길 바꾸니까 됐음
+                Pos f = fires.poll();
                 int fr = f.r;
                 int fc = f.c;
-
+//                System.out.println("fire! i는 : "+i+"   불의 위치 r " +fr+" c : "+fc);
                 for(int j=0;j<4;j++){
-                    int dfr = fr+rdir[j];
-                    int dfc = fc+cdir[j];
-                    //위치가 유효하고 불이 번지지 않았으면 이동
-                    //x로 방문처리
-                    if(dfc>0 && dfr>0 && dfc<W+1 && dfr<H+1 && visited[dfr][dfc] != -1){
-                        visited[dfr][dfc] = -1;
-                        fire.offer(new Pos(dfr, dfc));
+                    int dfr = fr + dr[j];
+                    int dfc = fc + dc[j];
+                    //위치가 유효하고 빈공간이거나 상근이가있으면 이동
+                    if(isIn(dfr, dfc, 'f') && (map[dfr][dfc]=='.' || map[dfr][dfc]=='@')){
+//                    if(isIn(dfr, dfc) && (map[dfr][dfc]=='.')){
+                        map[dfr][dfc] = '*';
+                        fires.offer(new Pos(dfr, dfc));
                     }
                 }
             }
 
             //상근이 이동
-            for(int i=0;i<queue.size();i++){
-                Pos p = queue.poll();
-                int r = p.r;
-                int c = p.c;
-                int time = p.time;
+            for(int i=0, end=q.size();i<end;i++){
+                Pos s = q.poll();
+                int sr = s.r;
+                int sc = s.c;
+                int time = s.time;
+//                System.out.println("상근이! i는 : "+i+"   상근이 위치 r " +sr+" c : "+sc+"   시간 : "+time);
 
-                //끝에 도달한 경우 min값 update
-                if(c==0||r==0||c==W+1||r==H+1){
-                    min = min>time? time:min;
+                //골인했을 때
+                if(sr==0 || sc==0 || sr==R+1 || sc==C+1){
+                    min = Math.min(min, time);
                     continue;
                 }
 
                 for(int j=0;j<4;j++){
-                    int dr = r+rdir[j];
-                    int dc = c+cdir[j];
-                    //범위가 유효하고 불이 퍼지지 않았고 상근이가 방문한 곳이 아니면 방문처리
-                    if(dc>=0 && dr>=0 && dc<=W+1 && dr<=H+1){
-                        if(visited[dr][dc] != -1 && visited[dr][dc] != 1){
-                            queue.offer(new Pos(dr, dc, time+1));
-                            visited[dr][dc] = 1;
-                        }
+                    int dsr = sr + dr[j];
+                    int dsc = sc + dc[j];
+                    //범위가 유효하고 불도 없고 상근이도 간 적 없을 때(빈곳일때)
+                    if(isIn(dsr,dsc, 's') && (map[dsr][dsc]=='.'||map[dsr][dsc]=='X')){
+                        q.offer(new Pos(dsr, dsc, time+1));
+                        map[dsr][dsc] = '@';
                     }
                 }
             }
 
+
         }
+    }
+
+
+    static boolean isIn(int r, int c, char type){
+        if(type=='f')  return r>0 && c>0 && r<=R && c<=C;
+        else if(type=='s') return r>=0 && c>=0 && r<=R+1 && c<=C+1;
+        return false;
     }
 
 
