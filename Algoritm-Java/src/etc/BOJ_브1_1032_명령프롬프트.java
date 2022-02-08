4e1e84c98c8ea19e40c53bc338b9363c722f154c
오혜린 <gpfls0506@gmail.com>
2022-02-08 19:26:10 +0900

    update

---
+++ Algoritm-Java/src/etc/BOJ_브1_1032_명령프롬프트.java
@@ -0,0 +1,40 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_브1_1032_명령프롬프트 {
+
+
+    static char[][] input;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        input = new char[N][];
+
+        for(int i=0;i<N;i++){
+            input[i] = br.readLine().toCharArray();
+        }
+        int len = input[0].length;
+        for(int c=0;c<len;c++){
+            boolean isSame=true;
+            for(int r=1;r<N;r++){
+                if(input[r-1][c]!=input[r][c]){
+                    isSame = false;
+                }
+            }
+            if(isSame){
+                sb.append(input[0][c]);
+            }else{
+                sb.append("?");
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+
+}
