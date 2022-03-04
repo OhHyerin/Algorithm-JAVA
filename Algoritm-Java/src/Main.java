ff9faa851cae7a74b563619e211fb2c89b5e7123
오혜린 <gpfls0506@gmail.com>
2022-03-04 19:15:01 +0900

    update

--- Algoritm-Java/src/Main.java
+++ Algoritm-Java/src/Main.java
@@ -8,19 +8,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
-
         StringBuilder sb = new StringBuilder();
 
 
-        while(true){
 
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            sb.append(a+b).append("\n");
-            if(!br.ready()) break;
-        }
-        System.out.println(sb);
+//
+//
+//        while(true){
+//
+//            st = new StringTokenizer(br.readLine());
+//            int a = Integer.parseInt(st.nextToken());
+//            int b = Integer.parseInt(st.nextToken());
+//            sb.append(a+b).append("\n");
+//            if(!br.ready()) break;
+//        }
+//        System.out.println(sb);
     }
 }
--- Algoritm-Java/src/implementation/BOJ_S3_9996_한국이그리울땐서버에접속하지.java
+++ Algoritm-Java/src/implementation/BOJ_S3_9996_한국이그리울땐서버에접속하지.java
@@ -10,12 +10,27 @@ public class BOJ_S3_9996_한국이그리울땐서버에접속하지 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
         String pattern = br.readLine();
+        int patterSize = pattern.length();
 
-        char[] alphabet = new char[26];
+        for(int i=0;i<n;i++){
+            String str = br.readLine();
 
+            int idx = 0;
+            for(int j=0;j<str.length();j++){
+                if(pattern.charAt(idx)=='*'){
+                    idx++;
+                    continue;
+                }
+                if(str.charAt(j)==pattern.charAt(idx)){
+
+                }
+            }
+
+        }
 
 
 
