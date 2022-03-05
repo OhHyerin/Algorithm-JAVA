0bdfc476c6d868baa8798fd0713f08fa84bf22e8
오혜린 <gpfls0506@gmail.com>
2022-03-06 00:17:12 +0900

    update

--- Algoritm-Java/src/implementation/BOJ_S3_9996_한국이그리울땐서버에접속하지.java
+++ Algoritm-Java/src/implementation/BOJ_S3_9996_한국이그리울땐서버에접속하지.java
@@ -10,29 +10,38 @@ public class BOJ_S3_9996_한국이그리울땐서버에접속하지 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
         String pattern = br.readLine();
-        int patterSize = pattern.length();
+        int starIdx = pattern.indexOf("*");
+//        System.out.println(starIdx);
+
+        String patternStart = pattern.substring(0, starIdx);  //pattern 처음부터 * 전까지
+        String patternEnd = pattern.substring(starIdx+1); //pattern *다음부터 끝까지
+
+        int patternStartSize = patternStart.length();
+        int patternEndSize = patternEnd.length();
 
         for(int i=0;i<n;i++){
             String str = br.readLine();
+            int strSize = str.length();
 
-            int idx = 0;
-            for(int j=0;j<str.length();j++){
-                if(pattern.charAt(idx)=='*'){
-                    idx++;
-                    continue;
-                }
-                if(str.charAt(j)==pattern.charAt(idx)){
 
+            if(strSize<pattern.length()-1){  //패턴사이즈보다 String길이가 짧으면
+                sb.append("NE").append("\n");
+            }else{
+                String strStart = str.substring(0, patternStartSize);
+                String strEnd = str.substring(str.length()-patternEndSize);
+                if(patternStart.equals(strStart) && patternEnd.equals(strEnd)){
+                    sb.append("DA").append("\n");
+                }else{
+                    sb.append("NE").append("\n");
                 }
             }
-
         }
-
-
+        System.out.println(sb);
 
 
     }
