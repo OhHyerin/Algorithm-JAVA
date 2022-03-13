4082116e2c9f1e3e49f4777662dab6725448a4e8
오혜린 <gpfls0506@gmail.com>
2022-03-13 23:28:55 +0900

    update

--- Algoritm-Java/src/minimum_spanning_tree/BOJ_G4_1976_여행가자.java
+++ Algoritm-Java/src/minimum_spanning_tree/BOJ_G4_1976_여행가자.java
@@ -1,6 +1,43 @@
 package minimum_spanning_tree;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_G4_1976_여행가자 {
     //백준 골드4
     //스터디 MST
+
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+
+
+
+    }
+
+    static class Node implements Comparable<Node>{
+        private int distance;
+        private int nodeA;
+        private int nodeB;
+
+        public Node(int nodeA, int nodeB, int distance) {
+            this.nodeA = nodeA;
+            this.nodeB = nodeB;
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return distance-o.distance;
+        }
+    }
 }
