d51ce9876e1dead6ab329a20f092b921b439ff56
오혜린 <gpfls0506@gmail.com>
2022-03-25 21:22:14 +0900

    update

--- Algoritm-Java/src/etc/BOJ_G4_14466_소가길을건너간이유6.java
+++ Algoritm-Java/src/etc/BOJ_G4_14466_소가길을건너간이유6.java
@@ -1,10 +1,24 @@
 package etc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_G4_14466_소가길을건너간이유6 {
     //백준 골드4
     //스터디 - 코테광탈
 
-    public static void main(String[] args) {
+    static int N; //N :
+    static int K;
+    static int R;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+
+
 
     }
 }
