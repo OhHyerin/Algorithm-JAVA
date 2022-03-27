ec651b627dbeca39736d198553371b2e2271f927
오혜린 <gpfls0506@gmail.com>
2022-03-28 01:34:33 +0900

    update

--- Algoritm-Java/src/etc/BOJ_G4_14466_소가길을건너간이유6.java
+++ Algoritm-Java/src/etc/BOJ_G4_14466_소가길을건너간이유6.java
@@ -3,7 +3,10 @@ package etc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class BOJ_G4_14466_소가길을건너간이유6 {
     //백준 골드4
@@ -12,12 +15,15 @@ public class BOJ_G4_14466_소가길을건너간이유6 {
     static int N; // N : 배열 크기
     static int K; // K : 소의 개수
     static int R; // R : 길의 개수
-    static ArrayList<Load> loads;
-    static int[][] map;
-    static int[] dr = {-1, 1, 0, 0};
+    static ArrayList<Pos> loads[][];  //길의 정보를 담고 있는 list
+    static int[][] map;  //전체 map (소가 있는 곳은 1)
+    static boolean[][] visited;  //bfs검사할 때 사용하는 visited
+    static int[] dr = {-1, 1, 0, 0}; 
     static int[] dc = {0, 0, -1, 1};
-    static Pos[] cows;
-    static int[] isSelected;
+    static int[] isSelected;  //combination으로 뽑힌 숫자 2개 저장하는 배열
+    static Cow[] cows;  //소 number, r, c 저장하는 배열
+    static int answer = 0;  //몇 쌍 연결되는지
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,8 +33,14 @@ public class BOJ_G4_14466_소가길을건너간이유6 {
         K = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
 
-        loads = new ArrayList<>();
-        map = new int[N+1][N+1];
+        loads = new ArrayList[N+1][N+1];  //길 정보
+        map = new int[N+1][N+1]; //전체 맵 (소가 있는 자리는 1)
+
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                loads[i][j] = new ArrayList<>();
+            }
+        }
 
         for(int i=0;i<R;i++){
             st = new StringTokenizer(br.readLine());
@@ -37,102 +49,111 @@ public class BOJ_G4_14466_소가길을건너간이유6 {
             int r2 = Integer.parseInt(st.nextToken());
             int c2 = Integer.parseInt(st.nextToken());
 
-            loads.add(new Load(r1, c1, r2, c2));
+            loads[r1][c1].add(new Pos(r2,c2));
+            loads[r2][c2].add(new Pos(r1,c1));
         }
 
-        cows = new Pos[K];
+        cows = new Cow[K];
         for(int i=0;i<K;i++){
             st = new StringTokenizer(br.readLine());
             int r = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
             map[r][c] = 1;  //소가 있는 자리는 1
-            cows[i] = new Pos(i+1, r, c);
+            cows[i] = new Cow(i+1, r, c);
         }
 
         isSelected = new int[2];
         combination(0, 0);
 
+        System.out.println(K-answer);
     }
 
     static void combination(int cnt, int start){
         if(cnt==2){
-            System.out.println(Arrays.toString(isSelected));
+            int fr=0, fc=0, tr=0,tc=0;
+            for(int i=0;i<cows.length;i++){
+                if(cows[i].num==isSelected[0]){
+                    fr = cows[i].r;
+                    fc = cows[i].c;
+                }else if(cows[i].num==isSelected[1]){
+                    tr = cows[i].r;
+                    tc = cows[i].c;
+                }
+            }
+            bfs(fr, fc, tr, tc);
             return;
         }
-
-        for(int i=start;i< cows.length;i++){
+        for(int i=start;i<cows.length;i++){
             isSelected[cnt] = cows[i].num;
             combination(cnt+1, i+1);
         }
-
     }
 
-    static void bfs(Pos a, Pos b){
-        //Queue를 전역변수로 바꿔보기
 
+
+    private static void bfs(int r, int c, int tr, int tc){
+        Queue<Pos> queue = new LinkedList<>();
+        visited = new boolean[N+1][N+1];
+
+        queue.add(new Pos(r, c));
+        visited[r][c] = true;
 
         while(!queue.isEmpty()){
             Pos cur = queue.poll();
-            for(int d=0;d<4;d++){
-                Pos next = new Pos(cur.r+dr[d], cur.c+dc[d]);
-                int nr = next.r;
-                int nc = next.c;
+            int cr = cur.r;
+            int cc = cur.c;
 
-                if(isIn(nr, nc)){
-                    if(!isLoad(cur, next)){
-                        queue.offer(new Pos(nr, nc));
+            if(cr==tr && cc==tc){
+                answer++;
+                return;
+            }
+
+            for(int d=0;d<4;d++){
+                int nr = cr+dr[d];
+                int nc = cc+dc[d];
+
+                boolean flag = true;
+
+                if(isIn(nr,nc)){
+                    if(!visited[nr][nc]){
+                        for(Pos tmp:loads[cr][cc]){
+                            if(tmp.r==nr && tmp.c==nc){
+                                flag = false; //불가
+                                continue;
+                            }
+                        }
+
+                        if(flag){
+                            //다리를 건너는게 아니면 이동
+                            queue.add(new Pos(nr, nc));
+                            visited[nr][nc] = true;
+                        }
                     }
                 }
-
             }
         }
-
     }
 
     static boolean isIn(int r, int c){
         return r>0 && c>0 && r<=N && c<=N;
     }
 
-
-    static boolean isLoad(Pos a, Pos b){
-        for(int i=0;i<loads.size();i++){
-            int r1 = loads.get(i).r1;
-            int c1 = loads.get(i).c1;
-            int r2 = loads.get(i).r2;
-            int c2 = loads.get(i).c2;
-
-            if(a.r==r1 && a.c==c1 && b.r==r2 && b.c==c2){
-                return true;
-            }
+    private static class Pos{
+        int r;
+        int c;
+        Pos(int r, int c){
+            this.r = r;
+            this.c = c;
         }
-        return false;
     }
 
-
-
-
-    static class Load{
-        int r1;
-        int c1;
-        int r2;
-        int c2;
-
-        public Load(int r1, int c1, int r2, int c2) {
-            this.r1 = r1;
-            this.c1 = c1;
-            this.r2 = r2;
-            this.c2 = c2;
-        }
-
-    }
-
-    static class Pos{
+    private static class Cow{
         int num;
         int r;
         int c;
 
-        public Pos(int num, int r, int c) {
+        public Cow(int num, int r, int c) {
             this.num = num;
             this.r = r;
             this.c = c;
---
+++ Algoritm-Java/src/etc/BOJ_G4_14466_소가길을건너간이유6_fail.java
@@ -0,0 +1,163 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_G4_14466_소가길을건너간이유6_fail {
+    //백준 골드4
+    //스터디 - 코테광탈
+    // 시간초과
+
+    static int N; // N : 배열 크기
+    static int K; // K : 소의 개수
+    static int R; // R : 길의 개수
+    static ArrayList<Load> loads;
+    static int[][] map;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static Pos[] cows;
+    static int[] isSelected;
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        loads = new ArrayList<>();
+        map = new int[N+1][N+1];
+
+        for(int i=0;i<R;i++){
+            st = new StringTokenizer(br.readLine());
+            int r1 = Integer.parseInt(st.nextToken());
+            int c1 = Integer.parseInt(st.nextToken());
+            int r2 = Integer.parseInt(st.nextToken());
+            int c2 = Integer.parseInt(st.nextToken());
+
+            loads.add(new Load(r1, c1, r2, c2));
+        }
+
+        cows = new Pos[K];
+        for(int i=0;i<K;i++){
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            map[r][c] = 1;  //소가 있는 자리는 1
+            cows[i] = new Pos(i+1, r, c);
+        }
+
+        isSelected = new int[2];
+        combination(0, 0);
+
+        System.out.println(count);
+
+    }
+
+    static void combination(int cnt, int start){
+        if(cnt==2){
+//            System.out.println(Arrays.toString(isSelected));
+            Pos a = null;
+            Pos b = null;
+            for(int i=0;i<cows.length;i++){
+                if(cows[i].num==isSelected[0]){
+                    a = cows[i];
+                }else if(cows[i].num==isSelected[1]){
+                    b = cows[i];
+                }
+            }
+
+            if(!bfs(a, b)) count++;
+            return;
+        }
+
+        for(int i=start;i< cows.length;i++){
+            isSelected[cnt] = cows[i].num;
+            combination(cnt+1, i+1);
+        }
+
+    }
+
+    static boolean bfs(Pos a, Pos b){
+
+        Queue<Pos> queue = new LinkedList<>();
+        queue.offer(a);
+
+        while(!queue.isEmpty()){
+            Pos cur = queue.poll();
+
+            if(cur.r==b.r && cur.c==b.c){
+                return true;
+            }
+
+
+            for(int d=0;d<4;d++){
+                Pos next = new Pos(cur.num, cur.r+dr[d], cur.c+dc[d]);
+                int nr = next.r;
+                int nc = next.c;
+
+                if(isIn(nr, nc)){
+                    if(!isLoad(cur, next)){
+                        queue.offer(new Pos(cur.num, nr, nc));
+                    }
+                }
+
+            }
+        }
+    return false;
+    }
+
+    static boolean isIn(int r, int c){
+        return r>0 && c>0 && r<=N && c<=N;
+    }
+
+
+    static boolean isLoad(Pos a, Pos b){
+        for(int i=0;i<loads.size();i++){
+            int r1 = loads.get(i).r1;
+            int c1 = loads.get(i).c1;
+            int r2 = loads.get(i).r2;
+            int c2 = loads.get(i).c2;
+
+            if(a.r==r1 && a.c==c1 && b.r==r2 && b.c==c2){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+    static class Load{
+        int r1;
+        int c1;
+        int r2;
+        int c2;
+
+        public Load(int r1, int c1, int r2, int c2) {
+            this.r1 = r1;
+            this.c1 = c1;
+            this.r2 = r2;
+            this.c2 = c2;
+        }
+
+    }
+
+    static class Pos{
+        int num;
+        int r;
+        int c;
+
+        public Pos(int num, int r, int c) {
+            this.num = num;
+            this.r = r;
+            this.c = c;
+        }
+    }
+}
