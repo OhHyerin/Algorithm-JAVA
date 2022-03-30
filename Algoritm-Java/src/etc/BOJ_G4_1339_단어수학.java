3cdc52957c4aa8d4fce258793604d2f044951d6e
오혜린 <gpfls0506@gmail.com>
2022-03-30 21:43:28 +0900

    update

--- Algoritm-Java/src/etc/BOJ_G4_1339_단어수학.java
+++ Algoritm-Java/src/etc/BOJ_G4_1339_단어수학.java
@@ -3,15 +3,12 @@ package etc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.*;
 
 public class BOJ_G4_1339_단어수학 {
     static int N;  //입력받을 문자열의 개수
     static String[] strs;  //입력받는 문자열
     static int diffCharCount = 0;
-    static int[] alpa;
     static char[] chars;
     static int[] ints;
     static int maxNum = Integer.MIN_VALUE;
@@ -23,7 +20,6 @@ public class BOJ_G4_1339_단어수학 {
 
         N = Integer.parseInt(br.readLine());
         strs = new String[N];
-        alpa = new int[26];
         hashSet = new HashSet<>();
 
         for(int i=0;i<N;i++){
@@ -32,12 +28,10 @@ public class BOJ_G4_1339_단어수학 {
 
         for(int i=0;i<N;i++){
             for(int j=0;j<strs[i].length();j++){
-                alpa[strs[i].charAt(j)-'A']++;
                 hashSet.add(strs[i].charAt(j));
             }
         }
 
-        System.out.println(Arrays.toString(alpa));
 
         diffCharCount = hashSet.size();
         chars = new char[diffCharCount];
@@ -49,17 +43,85 @@ public class BOJ_G4_1339_단어수학 {
             chars[index] = (char)iter.next();
             index++;
         }
+        for(int i=0;i<strs.length;i++){
+            for(int j=0;j<strs[i].length();j++){
+                char temp = strs[i].charAt(j);
+                for(int c=0;c<diffCharCount;c++){
+                    if(chars[c]==temp){
+                        ints[c]++;
+                    }
+                }
+            }
+        }
 
+//        System.out.println(Arrays.toString(chars));
+//        System.out.println(Arrays.toString(ints));
 
-        Arrays.sort(alpa);
-        System.out.println(Arrays.toString(alpa));
+        Queue<Chars> queue = new PriorityQueue<>();
+        for(int i=0;i<diffCharCount;i++){
+            queue.add(new Chars(chars[i], ints[i]));
+        }
+//        System.out.println(queue);
 
+        ArrayList<Chars> matching = new ArrayList<>();
+        for(int i=9;i>=(10-diffCharCount);i--){
+            Chars cur = queue.poll();
+            matching.add(new Chars(cur.ch, cur.count, i));
+        }
 
+//        System.out.println(matching);
 
-        System.out.println(Arrays.toString(chars));
-        System.out.println(Arrays.toString(ints));
+        String[] result = new String[strs.length];
+        for(int i=0;i<strs.length;i++){
+            result[i] = "";
+            for(int j=0;j<strs[i].length();j++){
+                char temp = strs[i].charAt(j);
+                for(int c=0;c<matching.size();c++){
+                    if(matching.get(c).ch==temp){
+                        result[i] += String.valueOf(matching.get(c).num);
+                    }
+                }
+            }
+        }
 
+        int answer = 0;
+        for(int i=0;i<strs.length;i++){
+                answer += Integer.parseInt(result[i]);
+        }
+
+        System.out.println(answer);
 //        System.out.println(maxNum);
 
+    }
+
+    static class Chars implements Comparable<Chars>{
+        char ch;
+        int count;
+        int num;
+
+        public Chars(char ch, int count) {
+            this.ch = ch;
+            this.count = count;
+        }
+
+        public Chars(char ch, int count, int num) {
+            this.ch = ch;
+            this.count = count;
+            this.num = num;
+        }
+
+        @Override
+        public int compareTo(Chars o) {
+            return -(count-o.count);
+        }
+
+        @Override
+        public String toString() {
+            return "Chars{" +
+                    "ch=" + ch +
+                    ", count=" + count +
+                    ", num=" + num +
+                    '}';
+        }
     }
 }
