0e9ff0268b538ed127ff65607a8607dbedf4d650
오혜린 <gpfls0506@gmail.com>
2022-04-03 19:27:10 +0900

    update

--- Algoritm-Java/src/etc/BOJ_G5_2174_로봇시뮬레이션.java
+++ Algoritm-Java/src/etc/BOJ_G5_2174_로봇시뮬레이션.java
@@ -8,10 +8,22 @@ import java.util.StringTokenizer;
 public class BOJ_G5_2174_·Îº¿½Ã¹Ä·¹ÀÌ¼Ç {
     //ÄÚÅ×±¤Å» ¹®Á¦Áý
 
+    static int A, B;
+    static int N, M;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+
 
     }
 }
