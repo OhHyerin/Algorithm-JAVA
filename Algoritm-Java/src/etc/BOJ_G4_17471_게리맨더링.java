fce4caa93cb97f4a17fb04e6d7c4781d8e1999cf
오혜린 <gpfls0506@gmail.com>
2022-04-06 10:40:47 +0900

    update

---
+++ Algoritm-Java/src/etc/BOJ_G4_17471_게리맨더링.java
@@ -0,0 +1,23 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_G4_17471_게리맨더링 {
+    //문제집 - 삼성A형 기출문제
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+
+
+
+    }
+
+
+}
---
+++ Algoritm-Java/src/implementation/BOJ_G4_2239_스도쿠.java
@@ -0,0 +1,97 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class BOJ_G4_2239_½ºµµÄí {
+
+    static int N = 9;
+    static int[][] map;
+    static StringBuilder sb = new StringBuilder();
+    static ArrayList<Blank> blanks;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        map = new int[N+1][N+1];
+        blanks = new ArrayList<>();
+
+        for(int i=0;i<N;i++){
+            String str = br.readLine();
+            for(int j=0;j<N;j++){
+                map[i][j] = str.charAt(j)-'0';
+                if(map[i][j]==0){
+                    blanks.add(new Blank(i, j));
+                }
+            }
+        }
+        fillblank(0);
+//        System.out.println(sb);
+
+    }
+
+    static void fillblank(int idx){
+
+        if(idx==blanks.size()){
+            for(int i=0;i<N;i++){
+                for(int j=0;j<N;j++){
+                    sb.append(map[i][j]);
+                }
+                sb.append("\n");
+            }
+            System.out.println(sb);
+            System.exit(0);
+        }
+
+        int r = blanks.get(idx).r;
+        int c = blanks.get(idx).c;
+
+
+        HashSet<Integer> hashSet = new HashSet<>();
+        for(int i=1;i<=9;i++){
+            hashSet.add(i);
+        }
+
+        for(int i=0;i<9;i++){
+            hashSet.remove(map[r][i]);
+            hashSet.remove(map[i][c]);
+        }
+
+        int sectionR = r/3*3;
+        int sectionC = c-c%3;
+
+        for(int i=sectionR;i<sectionR+3;i++){
+            for(int j=sectionC; j<sectionC+3;j++){
+                hashSet.remove(map[i][j]);
+            }
+        }
+
+        for(int i=1;i<10;i++){
+            if(hashSet.contains(i)){
+                map[r][c] = i;
+                fillblank(idx+1);
+                map[r][c] = 0;
+            }
+        }
+
+    }
+
+
+    static class Blank{
+        int r;
+        int c;
+
+        public Blank(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+
+
+}
