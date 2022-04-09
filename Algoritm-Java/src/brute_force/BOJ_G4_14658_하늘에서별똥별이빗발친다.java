712ff4bd2beca3cee4b57c70df2b739f07a32457
오혜린 <gpfls0506@gmail.com>
2022-04-10 04:08:20 +0900

    update

--- Algoritm-Java/src/brute_force/BOJ_G4_14658_하늘에서별똥별이빗발친다.java
+++ Algoritm-Java/src/brute_force/BOJ_G4_14658_하늘에서별똥별이빗발친다.java
@@ -1,9 +1,25 @@
 package brute_force;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_G4_14658_하늘에서별똥별이빗발친다 {
     //완전탐색
 
-    public static void main(String[] args) {
+    static int R, C;  //R*C
+    static int L;  //트램펄린 한 변의 길이
+    static int K; //별똥별의 수
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        C = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
     }
 
