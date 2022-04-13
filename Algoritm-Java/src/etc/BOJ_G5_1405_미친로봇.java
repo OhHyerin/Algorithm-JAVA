20efc7f34024ee01ce55abd3f076725618132113
오혜린 <gpfls0506@gmail.com>
2022-04-13 23:59:52 +0900

    update

--- Algoritm-Java/src/etc/BOJ_G5_1405_미친로봇.java
+++ Algoritm-Java/src/etc/BOJ_G5_1405_미친로봇.java
@@ -3,11 +3,23 @@ package etc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BOJ_G5_1405_¹ÌÄ£·Îº¿ {
 
+    static int n;
+    static int N, E, W, S;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken()); //µ¿
+        E = Integer.parseInt(st.nextToken()); //¼­
+        S = Integer.parseInt(st.nextToken()); //³²
+        N = Integer.parseInt(st.nextToken()); //ºÏ
+
 
 
     }
