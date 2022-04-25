9a71bdefb5f3aa3c1130e5dc4dbb80e3c2f795ca
오혜린 <gpfls0506@gmail.com>
2022-04-25 10:39:18 +0900

    update

--- Algoritm-Java/src/etc/BOJ_G5_2493_탑_2.java
+++ Algoritm-Java/src/etc/BOJ_G5_2493_탑_2.java
@@ -1,5 +1,16 @@
 package etc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_G5_2493_탑_2 {
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+    }
 }
