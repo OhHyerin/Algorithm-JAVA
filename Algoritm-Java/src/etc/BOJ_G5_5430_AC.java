97b4da541e14f9b9fb6a76e5d870743ebe3022aa
오혜린 <gpfls0506@gmail.com>
2022-05-02 02:34:04 +0900

    update

--- Algoritm-Java/src/etc/BOJ_G5_5430_AC.java
+++ Algoritm-Java/src/etc/BOJ_G5_5430_AC.java
@@ -3,6 +3,7 @@ package etc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BOJ_G5_5430_AC {
 
@@ -11,9 +12,30 @@ public class BOJ_G5_5430_AC {
     D : ¹ö¸®±â
      */
 
+    static char[] orders;
+    static int[] numbers;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
+        int T = Integer.parseInt(br.readLine());
+        for(int t=1;t<=T;t++){
+
+            String str = br.readLine();
+            orders = str.toCharArray();
+
+            int n = Integer.parseInt(br.readLine());
+            numbers = new int[n];
+
+            st = new StringTokenizer(br.readLine(), "[],");
+            for(int i=0;i<n;i++){
+                numbers[i] = Integer.parseInt(st.nextToken());
+            }
+
+
+        }//t
 
     }
 }
