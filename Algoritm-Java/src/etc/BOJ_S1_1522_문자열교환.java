6eba05c4666ec4ffcc319311bd895d23737cf2c0
오혜린 <gpfls0506@gmail.com>
2022-05-01 22:46:05 +0900

    update

--- Algoritm-Java/src/etc/BOJ_S1_1522_문자열교환.java
+++ Algoritm-Java/src/etc/BOJ_S1_1522_문자열교환.java
@@ -3,14 +3,17 @@ package etc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class BOJ_S1_1522_문자열교환 {
 
+    static String str;
+    static char[] chars;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
+        str = br.readLine();
+        chars = str.toCharArray();
 
     }
 }
