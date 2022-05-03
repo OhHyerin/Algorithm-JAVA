d36c201c296d86857467c1f6d2c40321423a4ee5
오혜린 <gpfls0506@gmail.com>
2022-05-03 19:56:17 +0900

    update

---
+++ Algoritm-Java/src/etc/BOJ_G4_4179_불.java
@@ -0,0 +1,7 @@
+package etc;
+
+public class BOJ_G4_4179_불 {
+    //그래프탐색
+
+
+}
--- Algoritm-Java/src/etc/BOJ_G5_5430_AC.java
+++ Algoritm-Java/src/etc/BOJ_G5_5430_AC.java
@@ -3,22 +3,33 @@ package etc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class BOJ_G5_5430_AC {
+    //문자열, deque
 
     /*
     R : 뒤집기
     D : 버리기
      */
 
+    /*
+    1
+    RD
+    1
+    [42]
+     */
+
     static char[] orders;
     static int[] numbers;
+    static Deque<Integer> deque;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
         for(int t=1;t<=T;t++){
@@ -27,17 +38,69 @@ public class BOJ_G5_5430_AC {
             orders = str.toCharArray();
 
             int n = Integer.parseInt(br.readLine());
-            numbers = new int[n];
 
             st = new StringTokenizer(br.readLine(), "[],");
+            deque = new ArrayDeque<>();
             for(int i=0;i<n;i++){
-                numbers[i] = Integer.parseInt(st.nextToken());
+                deque.add(Integer.parseInt(st.nextToken()));
             }
 
+            make();
 
         }//t
 
+        System.out.println(sb);
+
     }
 
-    private static String make()
+    private static void make(){
+        boolean start = true; //왼쪽부터 시작
+
+        for(int i=0;i<orders.length;i++){
+
+            if(orders[i]=='R'){  //뒤집기
+                start = !start;
+                continue;
+            }else if(orders[i]=='D'){
+                if(start){  //방향이 오른쪽일경우
+                    if(!deque.isEmpty()){
+                        deque.pollFirst();
+                    }else{
+                        sb.append("error\n");
+                        return;
+                    }
+                }else{
+                    if(!deque.isEmpty()){
+                        deque.pollLast();
+                    }else{
+                        sb.append("error\n");
+                        return;
+                    }
+                }
+            }
+        }
+
+        //남은거 String으로 변환
+        sb.append("[");
+
+        if(deque.isEmpty()){
+            sb.append("]\n");
+            return;
+        }
+
+        if(start) {
+            sb.append(deque.pollFirst());
+        }else{
+            sb.append(deque.pollLast());
+        }
+
+        while(deque.size()>0){
+            if(start) {
+                sb.append(",").append(deque.pollFirst());
+            }else{
+                sb.append(",").append(deque.pollLast());
+            }
+        }
+        sb.append("]\n");
+    }
 }
