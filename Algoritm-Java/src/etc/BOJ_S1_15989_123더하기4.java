7f1c53546422bba03aa2498c0cc03bc2ab13fd07
오혜린 <gpfls0506@gmail.com>
2022-05-06 17:05:17 +0900

    update

--- Algoritm-Java/src/etc/BOJ_S1_15989_123더하기4.java
+++ Algoritm-Java/src/etc/BOJ_S1_15989_123더하기4.java
@@ -1,9 +1,25 @@
 package etc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BOJ_S1_15989_123¥ı«œ±‚4 {
     //dp
 
-    public static void main(String[] args) {
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for(int t=1;t<=T;t++){
+
+            N = Integer.parseInt(br.readLine());
+
+
+        }//t
 
     }
 }
