d1813c8fa156750675b0d4068bdc11497ff7fefb
오혜린 <gpfls0506@gmail.com>
2022-05-17 00:43:25 +0900

    update

---
+++ Algoritm-Java/src/etc/Programmers_메뉴리뉴얼.java
@@ -0,0 +1,21 @@
+package etc;
+
+public class Programmers_메뉴리뉴얼 {
+
+    public static void main(String[] args) {
+        String[] order = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
+        int[] course={2, 3, 4};
+    }
+
+    public static String[] solution(String[] orders, int[] course){
+        String[] answer = {};
+
+
+
+
+
+        return answer;
+    }
+
+
+}
