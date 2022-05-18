66da87335bad7103f305d1d5f009cdda3ee40ec1
오혜린 <gpfls0506@gmail.com>
2022-05-18 21:25:19 +0900

    update

--- Algoritm-Java/src/etc/BOJ_G5_11509_풍선맞추기.java
+++ Algoritm-Java/src/etc/BOJ_G5_11509_풍선맞추기.java
@@ -3,15 +3,15 @@ package etc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ_G5_11509_풍선맞추기 {
     //그리디
 
     static int N;
-    static List<Integer> balloons;
+//    static List<Node> balloons;
+    static Queue<Node> balloons;
+//    static Node[] balloons;
     static int maxHeight = Integer.MIN_VALUE;
     static int maxIndex;
 
@@ -20,44 +20,60 @@ public class BOJ_G5_11509_풍선맞추기 {
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
-        balloons = new ArrayList<>();
+        balloons = new PriorityQueue<>();
+//        balloons = new ArrayList<>();
 
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++){
             int temp = Integer.parseInt(st.nextToken());
-            balloons.add(temp);
-            if(maxHeight<temp){
-                maxIndex = i;
-                maxHeight = temp;
-            }
+            balloons.add(new Node(i, temp));
         }
 
-//        System.out.println(maxIndex);
+//        balloons.stream().sorted();
+
         int count = 0;
 
-        while(balloons.size()>0){
-//            int idx = maxIndex;
+        while(!balloons.isEmpty()){
+//            Node cur = balloons.remove(0);
+            Node cur = balloons.poll();
             count++;
-            for(int i=maxIndex;i<balloons.size();i++){
-                if(balloons.get(i)==maxHeight){
-                    balloons.remove(i);
-                    maxHeight--;
-                    i--;
-                }
-            }
+            if(balloons.size()==0) break;
+//            Node next = balloons.get(0);
+            Node next = balloons.peek();
 
-//            System.out.println(balloons);
-//            break;
-
-            maxHeight = 0;
-            for(int i=0;i<balloons.size();i++){
-                if(maxHeight<balloons.get(i)){
-                    maxHeight = balloons.get(i);
-                    maxIndex = i;
-                }
+//            for(int i=1;i<balloons.size();i++){
+//                if(cur.value== next.value){
+//                    next = balloons.get(i);
+//                }else{
+//                    break;
+//                }
+//            }
+            if(next.value==cur.value-1 && next.idx>cur.idx){
+                //다음 값이 현재보다 value는 1작고, idx는 크다면
+                count--;
             }
+//            else if(next.value==cur.value){
+//
+//            }
         }
+
         System.out.println(count);
 
+    }
+
+    static class Node implements Comparable<Node>{
+        int idx;
+        int value;
+
+        public Node(int idx, int value) {
+            this.idx = idx;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            if(value==o.value) return (idx-o.idx);
+            return (value-o.value)*-1;
+        }
     }
 }
--- Algoritm-Java/src/etc/Programmers_메뉴리뉴얼.java
+++ Algoritm-Java/src/etc/Programmers_메뉴리뉴얼.java
@@ -5,6 +5,16 @@ public class Programmers_메뉴리뉴얼 {
     public static void main(String[] args) {
         String[] order = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
         int[] course={2, 3, 4};
+        String[] result = solution(order, course);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        sb.append(result[0]);
+        for(int i=1;i<result.length;i++){
+            sb.append("\",");
+            sb.append(result[i]).append("\"");
+        }
+        System.out.println(sb);
     }
 
     public static String[] solution(String[] orders, int[] course){
