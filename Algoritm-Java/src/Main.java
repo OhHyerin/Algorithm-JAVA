7446c93f84587ebc1d1f7c7c02234c1da5511a05
오혜린 <gpfls0506@gmail.com>
2022-06-17 02:53:40 +0900

    update

--- Algoritm-Java/src/Main.java
+++ Algoritm-Java/src/Main.java
@@ -1,24 +1,41 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class Main {
 
-    static int[] arr = {1, 5, 4, 2, 9, 7};
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        List<Integer> list = new ArrayList<>();
+        Date today = new Date();
+        SimpleDateFormat MONTH = new SimpleDateFormat("MM");
+        SimpleDateFormat YEAR = new SimpleDateFormat("yyyy");
+        int month = Integer.parseInt(MONTH.format(today));
+        int year = Integer.parseInt(YEAR.format(today));
 
-        int n = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-
-        for(int i=a;i<=b;i++){
-            int temp = arr[a];
+        for(int i=1;i<=12;i++){
+            if(i==2){
+                if(year%4==0 && year%100 !=0 || year%400==0){
+                    list.add(29);
+                }else{
+                    list.add(28);
+                }
+            }else if(i<=7 && i%2==1){
+                //1, 3, 5, 7¿ù
+                list.add(31);
+            }else if(i<=7){
+                //4, 6¿ù
+                list.add(30);
+            }else if(i%2==0){
+                //8, 10, 12¿ù
+                list.add(31);
+            }else{
+                list.add(30);
+            }
         }
+
+        System.out.println(list.get(month-1)+" days for "+year+"-"+month);
 
     }
 }
