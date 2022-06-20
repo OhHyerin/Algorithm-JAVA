cd19bd7059be4e65d945004ab66835d8f9acbbd0
오혜린 <gpfls0506@gmail.com>
2022-06-20 21:31:00 +0900

    update

---
+++ Algoritm-Java/src/etc/BOJ_G5_3151_합이0.java
@@ -0,0 +1,14 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_G5_3151_гуюл0 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
