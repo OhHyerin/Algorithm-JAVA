d5630a5b5c71c9af5536b5a0a398f9fdc7712487
오혜린 <gpfls0506@gmail.com>
2022-07-09 18:32:19 +0900

    udpate

---
+++ Algoritm-Java/src/etc/BOJ_G4_6597_트리복구.java
@@ -0,0 +1,64 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_G4_6597_트리복구 {
+    //트리, 그래프탐색, dfs
+    // BOJ G3 4256 트리 문제랑 동일
+
+    /*
+    예제 :
+    전위순회(pre) : D B A C E G F
+    중위순회(in)  : A B C D E F G
+
+     */
+
+    static char[] preorder;
+    static char[] inorder;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String input;
+        while(true){
+            String str = br.readLine();
+            if(str==null) break;
+
+            st = new StringTokenizer(str);
+            String str1 = st.nextToken();
+            String str2 = st.nextToken();
+
+            int size = str1.length();
+
+            preorder = new char[size];
+            inorder = new char[size];
+
+            preorder = str1.toCharArray();
+            inorder = str2.toCharArray();
+
+            makePostorder(0, 0, size);
+            sb.append("\n");
+
+        }
+
+        System.out.println(sb);
+
+
+    }
+
+    private static void makePostorder(int root, int start, int end){
+        for(int i = start; i<end ; i++){
+            //start부터 end까지 검사
+            if(preorder[root]==inorder[i]){
+                makePostorder(root+1, start, i);
+                makePostorder(root+i-start+1, i+1, end);
+                sb.append(preorder[root]);
+            }
+        }
+    }
+}
