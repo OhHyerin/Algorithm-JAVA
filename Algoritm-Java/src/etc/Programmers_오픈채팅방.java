1e5923acd3cfcbbd4792a1faaa779daccc21a189
오혜린 <gpfls0506@gmail.com>
2022-07-13 18:01:53 +0900

    update

---
+++ Algoritm-Java/src/etc/Programmers_오픈채팅방.java
@@ -0,0 +1,21 @@
+package etc;
+
+public class Programmers_¿ÀÇÂÃ¤ÆÃ¹æ {
+    //
+
+    public static void main(String[] args) {
+        String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
+
+        String[] answer = solution(record);
+        for(int i=0;i<answer.length;i++){
+            System.out.println(answer[i]);
+        }
+    }
+
+    static public String[] solution(String[] record){
+        String[] answer = {};
+
+
+        return answer;
+    }
+}
