e8249230936dfc4c02d35d368e0b4844ac0287ea
OHR <gpfls0506@gmail.com>
2022-07-18 12:04:59 +0900

    update

--- Algoritm-Java/src/etc/Programmers_표편집.java
+++ Algoritm-Java/src/etc/Programmers_표편집.java
@@ -18,30 +18,42 @@ public class Programmers_표편집 {
     public static void main(String[] args) {
         int n = 8;  //행 개수를 나타내는 정수
         int k = 2;  //처음에 선택된 행의 위치
-        String[] cmd = {"D 2","C","U 3","C","D 4","C","U 2","Z","Z"};
+        String[] cmd = {"D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z"};
 
         System.out.println(solution(n, k, cmd));
 
     }
 
-    static public String solution(int n, int k, String[] cmd){
+    static public String solution(int n, int k, String[] cmd) {
         String answer = "";
 
         List<Row> rows = new ArrayList<>();
-        for(int i=1;i<=n;i++){
+        for (int i = 1; i <= n; i++) {
             rows.add(new Row(i, true));
         }
 
+        int cur = k;
 
+        for (int i = 0; i < cmd.length; i++) {
+            if (cmd[i].charAt(0) == 'U') {
+                int cnt = cmd[i].charAt(2)-'0';
 
+            } else if (cmd[i].charAt(0) == 'D') {
+                int cnt = cmd[i].charAt(2)-'0';
 
+            } else if (cmd[i].charAt(0) == 'C') {
 
+            } else if (cmd[i].charAt(0) == 'Z') {
+
+            }
+
+        }
 
 
         return answer;
     }
 
-    static class Row implements Comparable<Row>{
+    static class Row implements Comparable<Row> {
         int idx;
         boolean exist;
 
@@ -52,7 +64,7 @@ public class Programmers_표편집 {
 
         @Override
         public int compareTo(Row o) {
-            return idx-o.idx;
+            return idx - o.idx;
         }
     }
 
