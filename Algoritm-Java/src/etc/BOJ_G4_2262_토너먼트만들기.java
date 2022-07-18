a6746d612c0828505c48aa4432a232da8202b4f4
OHR <gpfls0506@gmail.com>
2022-07-18 19:18:45 +0900

    update

---
+++ Algoritm-Java/src/etc/BOJ_G4_2262_토너먼트만들기.java
@@ -0,0 +1,71 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_G4_2262_토너먼트만들기 {
+    //그리디
+
+    /*
+    1. 랭킹이 가장 낮은 선수를 찾는다 (숫자가 가장 높은 선수)
+    2. 1에서 찾은 선수에서 좌, 우 선수와의 차이값 중 최솟값을 찾는다
+
+     */
+
+    static int N;  //선수의 수
+    static int[] rank;
+    static List<Integer> list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        rank = new int[N];
+        list = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+//            rank[i] = Integer.parseInt(st.nextToken());
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int min = 0;  //1번
+        int max = N;  //N번  //가장 랭킹이 낮은 (숫자가 높은) 선수
+
+        for(int i=0;i<N-1;i++){
+            int idx = list.indexOf(max);  //랭킹이 낮은 선수의 인덱스
+
+            //랭킹이 가장 낮은 선수가 가장 앞에 있을 경우
+            // => 그 뒤 선수와의 차이
+            if(idx == 0){
+                min += list.get(idx) - list.get(idx+1);
+            }
+
+            //랭킹이 가장 낮은 선수가 가장 끝에 있을 경우
+            //  => 그 앞 선수와의 차이
+            else if(idx==list.size()-1){
+                min += list.get(idx) - list.get(idx-1);
+            }
+
+            //랭킹이 가장 낮은 선수가 중앙 어딘가에 있을 경우
+            // => 앞, 뒤 선수중 차이가 작은 선수와 매칭
+            else{
+                min += Math.min(list.get(idx)-list.get(idx-1), list.get(idx)-list.get(idx+1));
+            }
+
+            list.remove(idx); //랭킹이 가장 높은 선수는 매칭 끝났으므로 제거
+            max--;
+        }
+
+        System.out.println(min);
+
+
+    }
+
+}
--- Algoritm-Java/src/etc/Programmers_파일명정렬.java
+++ Algoritm-Java/src/etc/Programmers_파일명정렬.java
@@ -5,11 +5,15 @@ import java.util.Queue;
 
 public class Programmers_파일명정렬 {
     //2018 카카오 BLIND RECRUITMENT
+    //test 4부터 런타임에러
 
     public static void main(String[] args) {
         String[] input = {"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"};
 
-        System.out.println(solution(input));
+        String[] answer = solution(input);
+        for(int i=0;i<answer.length;i++){
+            System.out.println(answer[i]);
+        }
     }
 
     static public String[] solution(String[] files){
@@ -23,9 +27,7 @@ public class Programmers_파일명정렬 {
         int idx = 0;
         while(!queue.isEmpty()){
             File cur = queue.poll();
-            StringBuilder sb = new StringBuilder();
-            sb.append(cur.head).append(cur.number).append(cur.tail);
-            answer[idx] = sb.toString();
+            answer[idx] = cur.result;
             idx++;
         }
 
@@ -36,6 +38,7 @@ public class Programmers_파일명정렬 {
         String head = "";
         String number = "";
         String tail ="";
+        String result = "";
 
         public File(String tmp) {
             int startIdx = 0;
@@ -51,27 +54,31 @@ public class Programmers_파일명정렬 {
                 }
                 if(isNum && (temp<'0' || temp>'9')){
                     endIdx = i;
+                    break;
                 }
             }
 
             this.head = tmp.substring(0, startIdx).toUpperCase();
             this.number = tmp.substring(startIdx, endIdx);
             this.tail = tmp.substring(endIdx, tmp.length());
+//            System.out.println("number : "+this.number);
 
+            this.result = tmp;
         }
 
         @Override
         public int compareTo(File o) {
-            int n = Integer.parseInt(this.number);
-            int m = Integer.parseInt(o.number);
-
-            if(n==m){
-                return tail.compareTo(o.tail);
-            }else if(head.equals(o.head)){
-                return n-m;
-            }else {
-                return head.compareTo(o.head);
+            if(this.head.compareTo(o.head)==0){
+                return Integer.parseInt(this.number)-Integer.parseInt(o.number);
             }
+            return this.head.compareTo(o.head);
+//            if(n==m){
+//                return tail.compareTo(o.tail);
+//            }else if(head.equals(o.head)){
+//                return n-m;
+//            }else {
+//                return head.compareTo(o.head);
+//            }
         }
     }
 }
