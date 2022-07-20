46956f033eda7d3cb7e78b45e3e6d04e7e59f735
오혜린 <gpfls0506@gmail.com>
2022-07-21 01:19:52 +0900

    update

--- Algoritm-Java/src/etc/BOJ_G3_17090_미로탈출하기.java
+++ Algoritm-Java/src/etc/BOJ_G3_17090_미로탈출하기.java
@@ -1,8 +1,10 @@
 package etc;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class BOJ_G3_17090_미로탈출하기 {
@@ -17,9 +19,10 @@ public class BOJ_G3_17090_미로탈출하기 {
 
     static int R, C;
     static char[][] map;
-    static int[][] dp;  //default : 0, 불가 : -1, 가능 : 1
-    static int count;
-//    static boolean[][] visited;
+    static boolean[][] dp;  //default : 0, 불가 : -1, 가능 : 1
+    static boolean[][] visited;
+    static Stack<Point> stack = new Stack<>();
+//    static HashSet<Point> possible = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,8 +32,8 @@ public class BOJ_G3_17090_미로탈출하기 {
         C = Integer.parseInt(st.nextToken());
 
         map = new char[R][C];
-        dp = new int[R][C];
-//        visited = new boolean[R][C];
+        dp = new boolean[R][C];
+        visited = new boolean[R][C];
 
         for(int i=0;i<R;i++){
             String str = br.readLine();
@@ -42,50 +45,80 @@ public class BOJ_G3_17090_미로탈출하기 {
 
 
         //--------------입력 완료-----------------------
-        for(int i=R-1;i>=0;i--){
-            for(int j=C-1;j>=0;j--){
-                if(dp[i][j]==-1) continue;
-                else if(dp[i][j]==1) {
-                    count++;
-                    continue;
-                }
-                dfs(i, j, i, j, new boolean[R][C]);
+        for(int i=0;i<R;i++){
+            for(int j=0;j<C;j++){
+                if(visited[i][j]) continue;
+
+//                dfs(i, j, i, j, 0);
+                dfs(i, j);
+
+//                System.out.println("i : "+i+"  j : "+j);
+//                for(int r=0;r<R;r++){
+//                    for(int c=0;c<C;c++){
+//                        System.out.print(visited[r][c]+" ");
+//                    }
+//                    System.out.println();
+//                }
+//                System.out.println("count : "+count);
+//                System.out.println();
+
             }
         }
 
-        System.out.println(count);
+        System.out.println(getCount());
 
     }
 
-    private static void dfs(int sr, int sc, int r, int c, boolean[][] visited){
-        if(!isIn(r, c) || dp[r][c]==1){
-            //범위 벗어나면 탈출, (sr, sc)위치의 dp값 1로 설정
-            count ++;
-            dp[sr][sc] = 1;
+//    private static void dfs(int sr, int sc, int r, int c, int cnt){
+    private static void dfs(int r, int c){
+        if(!isIn(r, c)){  //범위를 벗어났으면
+            while(!stack.isEmpty()){  //왔던길 모두 탐색하면서
+                Point pos = stack.pop();
+                dp[pos.x][pos.y] = true;  //가능으로 표시
+            }
             return;
         }
 
-        if(visited[r][c] || dp[r][c]==-1){
-            dp[sr][sc] = -1;
+        if(visited[r][c]){  //싸이클이 생기면
+//            dp[sr][sc] = -1;
+            while(!stack.isEmpty()){  //왔던길 모두 탐색하면서
+                Point pos = stack.pop();
+                dp[pos.x][pos.y] = dp[r][c];  //불가능으로 표시
+            }
             return;
         }
 
         visited[r][c] = true;
+        stack.push(new Point(r, c));  //경로 모두 stack에 넣어주기
 
         switch (map[r][c]){
             case 'U' :
-                dfs(sr, sc, r-1, c, visited);
+//                dfs(sr, sc, r-1, c,cnt+1);
+                dfs( r-1, c);
                 break;
             case 'R' :
-                dfs(sr, sc, r, c+1, visited);
+//                dfs(sr, sc, r, c+1,cnt+1);
+                dfs( r, c+1);
                 break;
             case 'D' :
-                dfs(sr, sc, r+1, c, visited);
+//                dfs(sr, sc, r+1, c,  cnt+1);
+                dfs( r+1, c);
                 break;
             case 'L' :
-                dfs(sr, sc, r, c-1, visited);
+//                dfs(sr, sc, r, c-1,cnt+1);
+                dfs( r, c-1);
                 break;
         }
+    }
+
+    private static int getCount(){
+        int count = 0;
+        for(int i=0;i<R;i++){
+            for(int j=0;j<C;j++){
+                if(dp[i][j]==true) count++;
+            }
+        }
+        return count;
     }
 
     private static boolean isIn(int r, int c){
---
+++ Algoritm-Java/src/etc/Programmers_몸짱트레이너라이언의고민.java
@@ -0,0 +1,21 @@
+package etc;
+
+public class Programmers_몸짱트레이너라이언의고민 {
+    //2017 카카오코드 본선
+
+    public static void main(String[] args) {
+        int[][] timetable = {{1170, 1210}, {1200, 1260}};
+        System.out.println(solution(3, 2, timetable));
+    }
+
+    static public int solution(int n, int m, int[][] timetable){
+        int answer = 0;
+
+
+
+
+
+        return answer;
+    }
+
+}
