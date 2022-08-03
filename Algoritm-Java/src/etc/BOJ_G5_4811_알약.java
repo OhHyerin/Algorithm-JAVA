7fe1f7065bef1b33282bff0ea13028c5d8028a7c
OHR <gpfls0506@gmail.com>
2022-08-04 01:32:42 +0900

    update

--- Algoritm-Java/src/etc/BOJ_G5_4811_알약.java
+++ Algoritm-Java/src/etc/BOJ_G5_4811_알약.java
@@ -9,21 +9,38 @@ public class BOJ_G5_4811_알약 {
 
 
     static int N;  //약의 개수수
-
+    static long[][] dp = new long[31][31];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         while (true) {
             int N = Integer.parseInt(br.readLine());
             if (N == 0) break;  //0이 입력되면 종료
 
-
-
-
-
+            sb.append(pill(N, 0)+"\n");
         }
 
+        System.out.println(sb.toString());
 
     }
+
+    public static long pill(int hole, int half){
+        if(hole==0){
+            //전체 알약 개수가 0개면
+            return 1;
+        }
+        if(dp[hole][half]>0){
+            //알약이 아직 남아있으면
+            return dp[hole][half];
+        }
+        dp[hole][half] = pill(hole-1, half+1);  //전체 하나 꺼내서 반은 다시 넣음
+        if(half>0){
+            dp[hole][half] += pill(hole, half-1);  //반개꺼내기
+        }
+        return dp[hole][half];
+    }
+
+
 
 }
