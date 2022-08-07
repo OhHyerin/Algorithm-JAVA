ecf967634220ad8a838e6bd0da8cd9c96a911e2a
OHR <gpfls0506@gmail.com>
2022-08-07 21:23:49 +0900

    update

---
+++ Algoritm-Java/src/etc/BOJ_G3_2513_통학버스.java
@@ -0,0 +1,103 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_G3_2513_통학버스 {
+    //그리디
+
+
+    static int N;  //아파트 단지의 수 (2<=N<=30000)
+    static int K;  //통학버스의 정원 (1<=K<=2000)
+    static int S;  //학교의 위치
+    static Queue<Pos> left;
+    static Queue<Pos> right;
+    static int dist;
+    static int vacanct;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        left = new PriorityQueue<>();
+        right = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int pos = Integer.parseInt(st.nextToken());
+            int cnt = Integer.parseInt(st.nextToken());
+
+            if (pos < S) {
+                left.add(new Pos(pos, cnt, "left"));
+            } else {
+                right.add(new Pos(pos, cnt, "right"));
+            }
+
+        }
+        dist += fillSeat(left);
+        vacanct = 0;
+        dist += fillSeat(right);
+
+        System.out.println(dist);
+
+    }
+
+    private static int fillSeat(Queue<Pos> list) {
+        int move = 0;
+        while (!list.isEmpty()) {
+            Pos cur = list.poll();
+            while (cur.cnt > 0) { //해당 아파트의 학생들 모두 태울때까지 반복
+                if (vacanct == 0) {  //남은자리 없으면
+                    //버스 새로 보내야 함 (현재가 가장 학교에서 먼 거리니까)
+                    move += Math.abs(cur.pos - S)*2;
+                    vacanct = K;  //모두 내리니까 K만큼의 좌석 생김
+                    continue;
+                }
+                if (cur.cnt > vacanct) {  //몇 명만 탈 수 있다면
+                    cur.cnt -= vacanct;  //남은 자리만큼만 학생들을 태움
+                    vacanct = 0;  //남은자리 0
+                } else if (cur.cnt < vacanct) {  //학생 모두 태울 수 있다면
+                    vacanct -= cur.cnt;  //공석 학생수만큼 채움
+                    cur.cnt = 0;  //모두 탐
+                }
+            }
+        }
+        return move;
+    }
+
+    private static class Pos implements Comparable<Pos> {
+        int pos;
+        int cnt;
+        String relpos;
+
+        public Pos(int pos, int cnt, String relpos) {
+            this.pos = pos;
+            this.cnt = cnt;
+            this.relpos = relpos;
+        }
+
+        @Override
+        public int compareTo(Pos o) {
+            if (relpos.equals("left")) {
+                return this.pos - o.pos;
+            } else {
+                return (this.pos - o.pos) * -1;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "Pos{" +
+                    "pos=" + pos +
+                    ", cnt=" + cnt +
+                    ", relpos='" + relpos + '\'' +
+                    '}';
+        }
+    }
+}
---
+++ Algoritm-Java/src/etc/Programmers_뉴스클러스터링.java
@@ -0,0 +1,98 @@
+package etc;
+
+import java.util.*;
+
+public class Programmers_뉴스클러스터링 {
+    //2018 카카오 블라인드 채용
+
+    /*
+    J(A, B) : 두 집합의 교집합 크기를 두 집합의 합집합 크기로 나눈 값
+    A와 B가 둘 다 공집합일 경우 나눗셈이 정의되지 않으니 J(A, B) = 1로 정의한다.
+
+     */
+    public static void main(String[] args) {
+        System.out.println(solution("FRANCE", "french"));
+        System.out.println(solution("handshake", "shake hands"));
+        System.out.println(solution("aa1+aa2", "AAAA12"));
+        System.out.println(solution("E=M*C^2", "e=m*c^2"));
+    }
+
+    static public int solution(String str1, String str2){
+        int answer = 0;
+        str1 = str1.toUpperCase();
+        str2 = str2.toUpperCase();
+
+        HashMap<String, Integer> hashMap1 = new HashMap<>();
+        HashMap<String, Integer> hashMap2 = new HashMap<>();
+
+        for(int i=0;i<str1.length()-1;i++){
+            if(str1.charAt(i) < 'A' || str1.charAt(i) >'Z') continue;
+            if(str1.charAt(i+1) < 'A' || str1.charAt(i+1) >'Z') continue;
+            String tmp = str1.charAt(i)+""+str1.charAt(i+1);
+
+            if(hashMap1.containsKey(tmp)){
+                hashMap1.put(tmp, hashMap1.get(tmp)+1);
+            }else{
+                hashMap1.put(tmp, 1);
+            }
+        }
+
+        for(int i=0;i<str2.length()-1;i++){
+            if(str2.charAt(i) < 'A' || str2.charAt(i) > 'Z') continue;
+            if(str2.charAt(i+1) < 'A' || str2.charAt(i+1) > 'Z') continue;
+            String tmp = str2.charAt(i)+""+str2.charAt(i+1);
+
+            if(hashMap2.containsKey(tmp)){
+                hashMap2.put(tmp, hashMap2.get(tmp)+1);
+            }else{
+                hashMap2.put(tmp, 1);
+            }
+        }
+
+//        Set<String> keySet = hashMap1.keySet();
+//        for(String key : keySet){
+//            System.out.println(key+" : "+hashMap1.get(key));
+//        }
+
+        int min = 0;
+        int max = 0;
+
+        Iterator<Map.Entry<String, Integer>> it = hashMap1.entrySet().iterator();
+        while(it.hasNext()){
+            Map.Entry<String, Integer> entrySet = (Map.Entry<String, Integer>) it.next();
+            String key = entrySet.getKey();
+            int value = entrySet.getValue();
+
+            if(hashMap2.containsKey(key)){
+                min += Math.min(hashMap2.get(key), value);
+            }
+            max += value;
+        }
+
+        it = hashMap2.entrySet().iterator();
+        while(it.hasNext()){
+            Map.Entry<String, Integer> entrySet = (Map.Entry<String, Integer>) it.next();
+            max += entrySet.getValue();
+        }
+
+        max -= min;
+
+//        System.out.println();
+//        System.out.println(min);
+//        System.out.println(max);
+
+        if(min==0 && max==0) return 65536;
+        if(min==0 && max!=0) min = 1;
+
+
+
+        double result = (double)min/(double)max;
+        answer = (int)Math.floor(result*65536);
+
+
+
+        return answer;
+    }
+
+
+}
