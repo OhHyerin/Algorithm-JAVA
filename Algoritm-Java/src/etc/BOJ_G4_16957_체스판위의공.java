a63408175cb6d60289d630a7ad21c44547ff1262
OHR <gpfls0506@gmail.com>
2022-08-10 02:46:09 +0900

    update

---
+++ Algoritm-Java/src/etc/BOJ_G4_16957_체스판위의공.java
@@ -0,0 +1,42 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_G4_16957_체스판위의공 {
+    //dp, 그래프탐색
+
+    static int R, C;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        map = new int[R][C];
+
+        for(int i=0;i<R;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<C;j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        //---------------입력 완료-------------------
+
+
+    }
+
+    private static class Pos{
+        int r;
+        int c;
+        int num;
+
+
+    }
+}
