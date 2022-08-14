9fb7882ef78fa724030793780f8ccd1c7015236e
OHR <gpfls0506@gmail.com>
2022-08-15 05:30:42 +0900

    update

---
+++ Algoritm-Java/src/etc/BOJ_G5_2212_센서.java
@@ -0,0 +1,21 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_G5_2212_센서 {
+    //그리디
+
+    static int N;
+    static int K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+    }
+}
